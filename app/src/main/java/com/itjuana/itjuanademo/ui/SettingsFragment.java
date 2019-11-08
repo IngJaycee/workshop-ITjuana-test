1ba6a2d216d5cf57ed82fa8ffddf35d60a6e71e5
Juan Carlos Torres <jutorres@insulet.com>
2019-11-08 15:07:39 -0800

    Settings

--- app/src/main/java/com/itjuana/itjuanademo/ui/SettingsFragment.java
+++ app/src/main/java/com/itjuana/itjuanademo/ui/SettingsFragment.java
@@ -3,18 +3,41 @@ package com.itjuana.itjuanademo.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.itjuana.itjuanademo.R;
+import com.itjuana.itjuanademo.viewmodel.MainViewModel;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingsFragment extends Fragment {
 
+    @BindView(R.id.btnSave)
+    Button btnSave;
+
+    @BindView(R.id.etName)
+    EditText etName;
+
+    @BindView(R.id.etDescription)
+    EditText etDescription;
+
+    private MainViewModel mainViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainViewModel = ViewModelProviders.of(requireActivity()).get(MainViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +46,16 @@ public class SettingsFragment extends Fragment {
 
         ButterKnife.bind(this, view);
 
+        btnSend.setOnClickListener(v -> {
+            String name = etName.getText().toString().trim();
+            String description = etDescription.getText().toString().trim();
+            if (name.length() > 2 && description.length() > 2) {
+                mainViewModel.addUser(name, description);
+                Toast.makeText(requireContext(), "data sent", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(requireContext(), "please fill all fealds with at least 2 chars", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
